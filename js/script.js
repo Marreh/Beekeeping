5b96f156d8aef87c4d21cc2ff7bdf1ca489b8e77
Marina Rehak <91294435+Marreh@users.noreply.github.com>
2021-10-27 21:23:05 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+ $(document).ready(function () {
+       $("div:even").css("border", "2px solid red");
+       $("div:odd").css("border", "2px solid yellow");
+       });
+
+<!--O nama-->
+
+$(document).ready(function(){
+  $("p").click(function(){
+    $(this).hide();
+  });
+});
+
+
+
+
